c05076bd66cd115cb7c7e70cbac4280fdee00c04
Angelo Almeida <angelo94.aa@gmail.com>
2023-03-02 12:42:43 -0500

    actualizando favoritos

--- lib/favoritos.dart
+++ lib/favoritos.dart
@@ -1,5 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
 class Favoritos extends StatefulWidget {
@@ -12,95 +11,83 @@ class Favoritos extends StatefulWidget {
 class _FavoritosState extends State<Favoritos> {
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      children: [
-        Container()
-      ],
-    );
-  }
-
-  // ignore: non_constant_identifier_names
-  Container Principal() {
     return Container(
-    margin: EdgeInsets.all(16),
-    width: 629,
-    height: 370,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(15),
-      image: DecorationImage(
-        image: AssetImage('assets/batman.jpg'),
-        fit: BoxFit.cover,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 15, top: 20),
+            child: Center(
+              child: Text('Favoritos',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(16),
+            width: double.infinity,
+            height: 400.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                image: DecorationImage(
+                    image: AssetImage('assets/batman.jpg'), fit: BoxFit.cover)),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  left: 20,
+                  top: 190,
+                  child: Text(
+                    'Batman',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 226,
+                  child: Text(
+                    'John Doe',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w200),
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 262,
+                  child: IconButton(
+                    icon: Icon(Icons.more_vert),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 322,
+                  child: ElevatedButton.icon(
+                    onPressed: (){},
+                    icon: Icon(Icons.play_arrow),
+                    label: Text('Reproducir', style: TextStyle(fontSize: 18),),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      )
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-    ),
-    child: Stack(
-      // ignore: prefer_const_literals_to_create_immutables
-      children: [
-        Positioned(
-          top: 190,
-          left: 20,
-          child: Text(
-            'Batman',
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 30),
-          ),
-        ),
-        Positioned(
-          top: 226,
-          left: 20,
-          child: Text(
-            'John Doe',
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.w200,
-                fontSize: 20),
-          ),
-        ),
-        Positioned(
-          top: 262,
-          left: 20,
-          child: IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {},
-            color: Colors.white,
-          ),
-        ),
-        Positioned(
-          top: 322,
-          left: 20,
-          child: ElevatedButton.icon(
-            onPressed: (){},
-            icon: Icon(Icons.play_arrow, color: Colors.black,),
-            label: Text('Reproducir', style: TextStyle(color: Colors.black),),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-            ),
-          ),
-        )
-      ],
-    ),
-    
-  );
-  }
-
-  Column text() {
-    return Column(
-      // ignore: prefer_const_literals_to_create_immutables
-      children: [
-        Padding(
-          padding: EdgeInsets.only(left: 15, top: 20),
-          child: Center(
-            child: Text(
-              'Favoritos',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
